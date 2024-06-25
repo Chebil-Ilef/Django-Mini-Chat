7913cee6d291a11e15cff3ddcddc42a6844d2266
Lost Elf <ilef.chebil@insat.ucar.tn>
2024-06-25 11:39:14 +0000

    edit: change to asgi.py

--- ChitChat/urls.py
+++ ChitChat/urls.py
@@ -3,7 +3,7 @@ from ChitChat import views as chat_views
 from django.contrib.auth.views import LogoutView, LoginView
 
 urlpatterns = [
-    path("", chat_views.chatPage, name="chatPage"),
+    path("", chat_views.chatPage, name="chat-page"),
     path("auth/login/", LoginView.as_view(template_name="chat/loginPage.html"), name="login-user"),
     path("auth/logout/", LogoutView.as_view(), name="logout-user"),
 
